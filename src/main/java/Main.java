697cc1818e86e0ea0bbcc7a16c03540cfef0dd55
MonKey <zbaxrl@foxmail.com>
2021-04-10 00:13:27 +0800

    Add Intellij-like look and feel

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,5 @@
 import com.formdev.flatlaf.FlatLightLaf;
+import com.formdev.flatlaf.intellijthemes.FlatLightFlatIJTheme;
 
 import javax.swing.*;
 
@@ -6,6 +7,7 @@ public class Main {
     public static void main(String[] args) {
         try {
             UIManager.setLookAndFeel(new FlatLightLaf());
+            FlatLightFlatIJTheme.install();
         } catch(Exception ex) {
             System.err.println("Failed to initialize LaF");
         }
