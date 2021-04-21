903bf87ab25d34e52b0fcdde40b250747e25a370
James-Hen <james-hen@outlook.com>
2021-04-21 23:42:51 +0800

    Update Toolbar GUI, finished style following.

--- src/main/java/org/xjtujavacourse/client/ColorComboBox.java
+++ src/main/java/org/xjtujavacourse/client/ColorComboBox.java
@@ -1,6 +1,7 @@
 package org.xjtujavacourse.client;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ItemEvent;
@@ -8,25 +9,42 @@ import java.awt.event.ItemListener;
 import java.util.HashMap;
 
 public class ColorComboBox extends JComboBox<String> implements ItemListener {
-    HashMap<String, Action> colorActionMap;
-    ColorComboBox() {
-        super();
-    }
+    private HashMap<String, Action> colorActionMap;
+    private HashMap<String, Color> stringColorMap;
+    private HashMap<Color, String> colorStringMap;
     ColorComboBox(Action[] actions) {
         super();
         colorActionMap = new HashMap<String, Action>();
+        stringColorMap = new HashMap<String, Color>();
+        colorStringMap = new HashMap<Color, String>();
         for (Action act : actions) {
             String str = act.getValue(Action.NAME).toString();
             addItem(str);
             colorActionMap.put(str, act);
+            Color actColor;
+            try {
+                actColor = (Color)Color.class.getField(str).get(Color.class);
+            } catch (Exception e) {
+                e.printStackTrace();
+                actColor = Color.BLACK;
+            }
+            stringColorMap.put(str, actColor);
+            colorStringMap.put(actColor, str);
         }
         addItemListener(this);
+    }
+
+    public Color getSelectedColor() {
+        return stringColorMap.get(getSelectedItem().toString());
+    }
+
+    public void setSelectedColor(Color c) {
+        setSelectedItem(colorStringMap.get(c));
     }
 
     @Override
     public void itemStateChanged(ItemEvent e) {
         if (e.getStateChange() == ItemEvent.SELECTED) {
-            System.err.println(e.getItem().toString());
             colorActionMap.get(e.getItem().toString()).actionPerformed(
                     new ActionEvent(e.getSource(), e.getID(), e.getItem().toString())
             );
--- src/main/java/org/xjtujavacourse/client/EditorFrame.java
+++ src/main/java/org/xjtujavacourse/client/EditorFrame.java
@@ -2,15 +2,18 @@ package org.xjtujavacourse.client;
 
 import javax.swing.*;
 import javax.swing.filechooser.FileSystemView;
+import javax.swing.text.AttributeSet;
 import javax.swing.text.Document;
+import javax.swing.text.StyleConstants;
 import javax.swing.text.StyledEditorKit;
 import javax.swing.text.html.HTMLEditorKit;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.event.*;
 import java.io.File;
+import java.util.Enumeration;
 
-public class EditorFrame extends JFrame implements ActionListener {
+public class EditorFrame extends JFrame implements ActionListener, MouseListener {
+    // Menu and menu options
     private JMenuBar menuBar;
 
     private JMenu fileMenu;
@@ -19,7 +22,13 @@ public class EditorFrame extends JFrame implements ActionListener {
     private JMenu helpMenu;
     private JMenuItem debugMenu, aboutMenu;
 
+    // Editor container and style tools
     private JTextPane textArea;
+    private JPanel toolPanel;
+    private TextStyleCheckbox boldCheckBox, italicCheckBox;
+    private ColorComboBox textColorCombo;
+    private HTMLEditorKit textKit;
+
     private DebugFrame debugFrame;
 
     private File documentFile;
@@ -61,28 +70,38 @@ public class EditorFrame extends JFrame implements ActionListener {
         BorderLayout layout = new BorderLayout();
         this.setLayout(layout);
 
-        HTMLEditorKit textKit = new HTMLEditorKit();
+        textKit = new HTMLEditorKit();
         Document defaultDocument = textKit.createDefaultDocument();
         textArea = new JTextPane();
         textArea.setEditorKit(textKit);
+        textArea.addMouseListener(this);
         textArea.setDocument(defaultDocument);
         JScrollPane scroll = new JScrollPane(textArea);
         this.add(scroll, BorderLayout.CENTER);
 
         textArea.setContentType("text/html");
 
-        JPanel toolPanel = new JPanel();
+        toolPanel = new JPanel();
         this.add(toolPanel, BorderLayout.NORTH);
-        TextStyleCheckbox boldCheckBox =
+        boldCheckBox =
                 new TextStyleCheckbox("Bold", false, new StyledEditorKit.BoldAction());
-        TextStyleCheckbox italicCheckBox =
+        italicCheckBox =
                 new TextStyleCheckbox("Italic", false, new StyledEditorKit.ItalicAction());
-        ColorComboBox textColorCombo = new ColorComboBox(
+
+        textColorCombo = new ColorComboBox(
                 new Action[] {
-                        new StyledEditorKit.ForegroundAction("Black", Color.BLACK),
-                        new StyledEditorKit.ForegroundAction("Red", Color.RED),
-                        new StyledEditorKit.ForegroundAction("Green", Color.GREEN),
-                        new StyledEditorKit.ForegroundAction("Blue", Color.BLUE)
+                        new StyledEditorKit.ForegroundAction("BLACK", Color.BLACK),
+                        new StyledEditorKit.ForegroundAction("DARK_GRAY", Color.DARK_GRAY),
+                        new StyledEditorKit.ForegroundAction("LIGHT_GRAY", Color.LIGHT_GRAY),
+                        new StyledEditorKit.ForegroundAction("RED", Color.RED),
+                        new StyledEditorKit.ForegroundAction("GREEN", Color.GREEN),
+                        new StyledEditorKit.ForegroundAction("BLUE", Color.BLUE),
+                        new StyledEditorKit.ForegroundAction("GRAY", Color.GRAY),
+                        new StyledEditorKit.ForegroundAction("CYAN", Color.CYAN),
+                        new StyledEditorKit.ForegroundAction("MAGENTA", Color.MAGENTA),
+                        new StyledEditorKit.ForegroundAction("ORANGE", Color.ORANGE),
+                        new StyledEditorKit.ForegroundAction("PINK", Color.PINK),
+                        new StyledEditorKit.ForegroundAction("YELLOW", Color.YELLOW)
                 }
         );
         toolPanel.add(boldCheckBox);
@@ -92,6 +111,40 @@ public class EditorFrame extends JFrame implements ActionListener {
         // Setup debug frame
         debugFrame = new DebugFrame(textArea);
         defaultDocument.addDocumentListener(debugFrame);
+    }
+
+    private void refreshStyleToolStatus() {
+        AttributeSet attributeSet = textKit.getInputAttributes();
+        if (!StyleConstants.getForeground(attributeSet).equals(textColorCombo.getSelectedColor())) {
+            textColorCombo.setSelectedColor(StyleConstants.getForeground(attributeSet));
+        }
+        boldCheckBox.setSelected(StyleConstants.isBold(attributeSet));
+        italicCheckBox.setSelected(StyleConstants.isItalic(attributeSet));
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        refreshStyleToolStatus();
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+        refreshStyleToolStatus();
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+        refreshStyleToolStatus();
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+        refreshStyleToolStatus();
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+        refreshStyleToolStatus();
     }
 
     private class FileMenuListener implements ActionListener {
